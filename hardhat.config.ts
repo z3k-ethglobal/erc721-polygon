7dfb19f82f6d30f5fe96d2fc3b3a841e54f578bc
Aryan Godara <aryangodara03@gmail.com>
2023-12-09 21:35:53 +0530

    update hardhat config for polygon mumbai

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,28 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+// import { HardhatUserConfig } from "hardhat/config";
+// import "@nomicfoundation/hardhat-toolbox";
+// import * as dotenv from "dotenv";
 
-const config: HardhatUserConfig = {
-  solidity: "0.8.19",
-};
+// const { API_URL, PRIVATE_KEY } = process.env;
 
-export default config;
+// const config: HardhatUserConfig = {
+//   solidity: "0.8.17",
+// };
+
+// export default config;
+
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+
+const { API_URL, PRIVATE_KEY } = process.env;
+
+module.exports = {
+   solidity: "0.8.9",
+   defaultNetwork: "polygon_mumbai",
+   networks: {
+      hardhat: {},
+      polygon_mumbai: {
+         url: API_URL,
+         accounts: [`0x${PRIVATE_KEY}`]
+      }
+   },
+}
